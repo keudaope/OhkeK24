38dd238ba2019d9e5233335ba48fa3041b81ff06
Jyri Lindroos <jyri.lindroos@keuda.fi>
2024-01-19 08:11:39 +0200

    asd fasdf

--- T10/T10/Form1.Designer.cs
+++ T10/T10/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace T10
 {
-    partial class Form1
+    partial class NoppaFM
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.OtsikkoLB.Location = new System.Drawing.Point(26, 19);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(221, 45);
+            this.OtsikkoLB.TabIndex = 0;
+            this.OtsikkoLB.Text = "Nopan heitto";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 99);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // NoppaFM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Name = "NoppaFM";
+            this.Text = "Heitä noppaa";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label OtsikkoLB;
+        private PictureBox pictureBox1;
     }
 }
--- T10/T10/Form1.cs
+++ T10/T10/Form1.cs
@@ -1,8 +1,8 @@
 namespace T10
 {
-    public partial class Form1 : Form
+    public partial class NoppaFM : Form
     {
-        public Form1()
+        public NoppaFM()
         {
             InitializeComponent();
         }
