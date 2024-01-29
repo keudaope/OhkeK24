2e55bf7354244522f68978f659e7d87c9e9c65a0
Jyri Lindroos <jyri.lindroos@keuda.fi>
2024-01-29 14:56:20 +0200

    t18 TOIMIVA

---
+++ T18-Oppilaitoxet/T18-Oppilaitoxet/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace T18_Oppilaitoxet
+{
+    partial class AvainhenkilotForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.oppilaitoksetCB = new System.Windows.Forms.ComboBox();
+            this.katuosoiteLB = new System.Windows.Forms.Label();
+            this.PostinumeroLB = new System.Windows.Forms.Label();
+            this.PostitoimipaikkaLB = new System.Windows.Forms.Label();
+            this.PuhelinLB = new System.Windows.Forms.Label();
+            this.vastuuhloCB = new System.Windows.Forms.ComboBox();
+            this.phoneLB = new System.Windows.Forms.Label();
+            this.emailLB = new System.Windows.Forms.Label();
+            this.sijaintiLB = new System.Windows.Forms.Label();
+            this.titteliLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oppilaitosten yhteystiedot";
+            // 
+            // oppilaitoksetCB
+            // 
+            this.oppilaitoksetCB.FormattingEnabled = true;
+            this.oppilaitoksetCB.Location = new System.Drawing.Point(59, 114);
+            this.oppilaitoksetCB.Name = "oppilaitoksetCB";
+            this.oppilaitoksetCB.Size = new System.Drawing.Size(265, 33);
+            this.oppilaitoksetCB.TabIndex = 1;
+            this.oppilaitoksetCB.SelectedIndexChanged += new System.EventHandler(this.oppilaitoksetCB_SelectedIndexChanged);
+            // 
+            // katuosoiteLB
+            // 
+            this.katuosoiteLB.AutoSize = true;
+            this.katuosoiteLB.Location = new System.Drawing.Point(54, 160);
+            this.katuosoiteLB.Name = "katuosoiteLB";
+            this.katuosoiteLB.Size = new System.Drawing.Size(70, 25);
+            this.katuosoiteLB.TabIndex = 2;
+            this.katuosoiteLB.Text = "label2";
+            // 
+            // PostinumeroLB
+            // 
+            this.PostinumeroLB.AutoSize = true;
+            this.PostinumeroLB.Location = new System.Drawing.Point(54, 219);
+            this.PostinumeroLB.Name = "PostinumeroLB";
+            this.PostinumeroLB.Size = new System.Drawing.Size(70, 25);
+            this.PostinumeroLB.TabIndex = 3;
+            this.PostinumeroLB.Text = "label3";
+            // 
+            // PostitoimipaikkaLB
+            // 
+            this.PostitoimipaikkaLB.AutoSize = true;
+            this.PostitoimipaikkaLB.Location = new System.Drawing.Point(59, 281);
+            this.PostitoimipaikkaLB.Name = "PostitoimipaikkaLB";
+            this.PostitoimipaikkaLB.Size = new System.Drawing.Size(70, 25);
+            this.PostitoimipaikkaLB.TabIndex = 4;
+            this.PostitoimipaikkaLB.Text = "label4";
+            // 
+            // PuhelinLB
+            // 
+            this.PuhelinLB.AutoSize = true;
+            this.PuhelinLB.Location = new System.Drawing.Point(59, 345);
+            this.PuhelinLB.Name = "PuhelinLB";
+            this.PuhelinLB.Size = new System.Drawing.Size(70, 25);
+            this.PuhelinLB.TabIndex = 5;
+            this.PuhelinLB.Text = "label5";
+            // 
+            // vastuuhloCB
+            // 
+            this.vastuuhloCB.FormattingEnabled = true;
+            this.vastuuhloCB.Location = new System.Drawing.Point(473, 114);
+            this.vastuuhloCB.Name = "vastuuhloCB";
+            this.vastuuhloCB.Size = new System.Drawing.Size(265, 33);
+            this.vastuuhloCB.TabIndex = 6;
+            this.vastuuhloCB.TextChanged += new System.EventHandler(this.vastuuhloCB_TextChanged);
+            // 
+            // phoneLB
+            // 
+            this.phoneLB.AutoSize = true;
+            this.phoneLB.Location = new System.Drawing.Point(473, 345);
+            this.phoneLB.Name = "phoneLB";
+            this.phoneLB.Size = new System.Drawing.Size(70, 25);
+            this.phoneLB.TabIndex = 10;
+            this.phoneLB.Text = "label5";
+            // 
+            // emailLB
+            // 
+            this.emailLB.AutoSize = true;
+            this.emailLB.Location = new System.Drawing.Point(473, 281);
+            this.emailLB.Name = "emailLB";
+            this.emailLB.Size = new System.Drawing.Size(70, 25);
+            this.emailLB.TabIndex = 9;
+            this.emailLB.Text = "label4";
+            // 
+            // sijaintiLB
+            // 
+            this.sijaintiLB.AutoSize = true;
+            this.sijaintiLB.Location = new System.Drawing.Point(468, 219);
+            this.sijaintiLB.Name = "sijaintiLB";
+            this.sijaintiLB.Size = new System.Drawing.Size(70, 25);
+            this.sijaintiLB.TabIndex = 8;
+            this.sijaintiLB.Text = "label3";
+            // 
+            // titteliLB
+            // 
+            this.titteliLB.AutoSize = true;
+            this.titteliLB.Location = new System.Drawing.Point(468, 160);
+            this.titteliLB.Name = "titteliLB";
+            this.titteliLB.Size = new System.Drawing.Size(70, 25);
+            this.titteliLB.TabIndex = 7;
+            this.titteliLB.Text = "label2";
+            // 
+            // AvainhenkilotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 601);
+            this.Controls.Add(this.phoneLB);
+            this.Controls.Add(this.emailLB);
+            this.Controls.Add(this.sijaintiLB);
+            this.Controls.Add(this.titteliLB);
+            this.Controls.Add(this.vastuuhloCB);
+            this.Controls.Add(this.PuhelinLB);
+            this.Controls.Add(this.PostitoimipaikkaLB);
+            this.Controls.Add(this.PostinumeroLB);
+            this.Controls.Add(this.katuosoiteLB);
+            this.Controls.Add(this.oppilaitoksetCB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "AvainhenkilotForm";
+            this.Text = "Oppilaitosten yhteystiedot";
+            this.Load += new System.EventHandler(this.AvainhenkilotForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox oppilaitoksetCB;
+        private System.Windows.Forms.Label katuosoiteLB;
+        private System.Windows.Forms.Label PostinumeroLB;
+        private System.Windows.Forms.Label PostitoimipaikkaLB;
+        private System.Windows.Forms.Label PuhelinLB;
+        private System.Windows.Forms.ComboBox vastuuhloCB;
+        private System.Windows.Forms.Label phoneLB;
+        private System.Windows.Forms.Label emailLB;
+        private System.Windows.Forms.Label sijaintiLB;
+        private System.Windows.Forms.Label titteliLB;
+    }
+}
+
