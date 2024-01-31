40723b6150a4f58bcff20ab003a4e66a77acc2a4
Jyri Lindroos <jyri.lindroos@keuda.fi>
2024-01-31 13:06:37 +0200

    T19 valmis

--- T19-Kahvilax/T19-Kahvilax/Form1.cs
+++ T19-Kahvilax/T19-Kahvilax/Form1.cs
@@ -5,6 +5,43 @@ namespace T19_Kahvilax
         public Form1()
         {
             InitializeComponent();
+            meistaPL.BringToFront();
+            meistaLB.Text = "Keudan oppilaskunnan kahvila palvelee luokassa 320 välituntisin \n";
+            meistaLB.Text += "9:00 - 14:30 joka päivä maanantaista torstaihin ja perjantaisin \n";
+            meistaLB.Text += "9:00 - 12:30. \n\nKeudan oppilaskunnan kahvilasta saa lämpimien \n";
+            meistaLB.Text += "juomien lisäksi virvokkeita sekä pientä purtavaa sekä makeisia \n";
+            meistaLB.Text += "Tervetuloa tutustumaan!";
+            meistaLB.Font = new Font("Arial", 12);
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void meistaBT_Click(object sender, EventArgs e)
+        {
+            meistaPL.BringToFront();
+        }
+
+        private void ruoatBT_Click(object sender, EventArgs e)
+        {
+            ruoatPL.BringToFront();
+        }
+
+        private void juomatBT_Click(object sender, EventArgs e)
+        {
+            juomatPL.BringToFront();
+        }
+
+        private void herkutBT_Click(object sender, EventArgs e)
+        {
+            herkutPL.BringToFront();
+        }
+
+        private void koriBT_Click(object sender, EventArgs e)
+        {
+            KoriPL.BringToFront();
         }
     }
 }
